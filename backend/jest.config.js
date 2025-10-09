ace98f644be168f90898dedfc0847e6c862dc8dc
Jin-Ho Lee <jinho.michael.lee@gmail.com>
2025-10-09 19:10:25 +0100

    Move setup for in-memory MongoDB testing out of test folder and update Jest configuration

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.js'],
-  // You can add more options here (coverage, custom matchers, etc.)
+  setupFilesAfterEnv: ['<rootDir>/src/testSetup/setup.js'],
+  testMatch: ['**/?(*.)+(test).js'],
 };
